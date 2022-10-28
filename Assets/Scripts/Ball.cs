2e0c4a47597bb21a83c023705a3f6cb54e675d75
Gocer615 <gtumayy21@gmail.com>
2022-10-28 10:05:11 +0300

    fix: paint effect transform

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -22,7 +22,8 @@ public class Ball : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("cylinder"))
         {
-            Instantiate(splashParticle, transform.position, transform.rotation);
+            GameObject Splash = Instantiate(splashParticle, transform.position, transform.rotation);
+            Splash.transform.parent = collision.gameObject.transform;
             _rigidbody.AddForce(Vector3.up * jumpForce);
         }
         else
